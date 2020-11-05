7d2c5219a0d751c0db1e9befa73c7a6034f893ef
Fourcade Antoine <a.fourcade65@gmail.com>
2020-11-05 15:28:15 +0100

    Affichage via fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,30 @@
-const images = require('../img/images.json')
-
 let cardRow = $('.card-group')
 let cards = ' '
-for(let image of images) {
-    cards = (
-        '<div class="col-md-3">'
-        + '<div class="card mb-3" style="max-width: 18rem;">'
-            + `<img src="${image.chemin}" class="card-img-top" alt="...">`
-            + '<div class="card-body">'
-                + `<h5 class="card-title">${image.alt}</h5>`
-                + `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>`
-                + '<a href="#" class="btn btn-primary">Go somewhere</a>'
+
+function afficher(items) {
+    for(let item of items) {
+        cards = (
+            '<div class="col-md-3">'
+            + '<div class="card mb-3" style="max-width: 18rem;">'
+                + `<img src="${item.chemin}" class="card-img-top" alt="...">`
+                + '<div class="card-body">'
+                    + `<h5 class="card-title">${item.alt}</h5>`
+                    + `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>`
+                    + '<a href="#" class="btn btn-primary">Go somewhere</a>'
+                + '</div>'
             + '</div>'
-        + '</div>'
-    +'</div>');
-    cardRow.append(cards)
+        +'</div>');
+        cardRow.append(cards)
+    }
 }
+
+fetch('https://raw.githubusercontent.com/Nearrivers/Galerie/master/img/images.json')
+.then((response) => {
+    return response.json()
+}).then((res) => {
+    console.log(res);
+    afficher(res)
+})
+.catch((err) => {
+    console.log(err);
+})
